5b78ad761197d631db1c55e2b24a744caedf4b3b
paulogalindojr <55253406+paulogalindojr@users.noreply.github.com>
2019-10-15 13:42:13 -0300

    Add files via upload

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -7,12 +7,6 @@
   function BillingCycleController($http) {
     const vm = this
 
-    vm.cicloPagamento = {
-      nome: "paulo",
-      mes: 1,
-      ano: 2017
-    }
-
     vm.create = function() {
       const url = 'http://localhost:3003/api/billingCycles'
       $http.post(url, vm.billingCycles).then(function(response) {
