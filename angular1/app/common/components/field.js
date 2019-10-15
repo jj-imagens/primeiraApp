81daf55fdbeb8c671d9fa7aee3a2a166cb924db6
paulogalindojr <55253406+paulogalindojr@users.noreply.github.com>
2019-10-15 13:40:53 -0300

    Add files via upload

--- angular1/app/common/components/field.js
+++ angular1/app/common/components/field.js
@@ -1,24 +1,28 @@
-(function() {
+//
+( function () {
   angular.module('primeiraApp').component('field', {
     bindings: {
       id: '@',
       label: '@',
       grid: '@',
       placeholder: '@',
-    },
+      type: '@',
+      model: '=',
+        },
     controller: [
       'gridSystem',
-      function(gridSystem) {
-        this.gridClasses = gridSystem.toCssClasses(this.grid)
+      function(gridSystem){
+        this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
       }
-    ],
-    template: `
-     <div class="{{ $ctrl.gridClasses }}">
-       <div class="form-group">
-         <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
-         <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}"/>
-       </div>
-     </div>
+    ], // double mustache {{ interpolar o valor da variável e jogar aqui dentro }}
+    template:`
+    <div class="{{ $ctrl.gridClasses }}">
+          <div class="form-group">
+            <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
+            <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}"
+              type= "{{ $ctrl.type }}" ng-model="$ctrl.model"/>
+          </div>
+        </div>
     `
   })
-})()
+}) ()
